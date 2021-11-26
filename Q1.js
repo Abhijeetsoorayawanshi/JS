bed5fdc415821031c54220def0f1f99a4b9ebab9
Abhijeetsoorayawanshi <91652950+Abhijeetsoorayawanshi@users.noreply.github.com>
2021-11-26 14:53:15 +0530

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,43 @@
+// creating an object "Person" using literal
+var Person = {
+    firstName : "First name is person's first property",
+    lastName : "Last name is person's second property",
+    officeaddress : "Office address is person's third property",
+};
+console.log(Person);
+
+
+// creating an object "Person 1" using literal
+var Person1 = {
+    firstName : "Abhijeet",
+    lastName : "Kumar",
+    officeaddress : "Boring Road,Patna",
+};
+console.log("Person 1");
+console.log(Person1.firstName);
+console.log(Person1.lastName);
+console.log(Person1.officeaddress);
+
+
+// creating an object "Person 2" using literal
+var Person2 = {
+    firstName : "Suraj",
+    lastName : "Sinha",
+    officeaddress : "Kankarbagh,Patna",
+};
+console.log("Person 2");
+console.log(Person2.firstName);
+console.log(Person2.lastName);
+console.log(Person2.officeaddress);
+
+
+// creating an object "Person 3" using literal
+var Person3 = {
+    firstName : "Yash",
+    lastName : "Singh",
+    officeaddress : "Bazar samiti,Patna",
+};
+console.log("Person 3");
+console.log(Person3.firstName);
+console.log(Person3.lastName);
+console.log(Person3.officeaddress);
